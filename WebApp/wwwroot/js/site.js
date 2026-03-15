1ee8aa3f4d365df9c29bc75f0c1251678851b83e
JaredMJ1 <jamen1904@gmail.com>
2026-03-15 00:53:13 -0600

    se agrega la funcion a site.js

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const API_BASE_URL = "https://ecommerce-api-jared-axf5e0ebbrf5gae0.canadacentral-01.azurewebsites.net";
